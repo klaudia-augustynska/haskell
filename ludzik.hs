47610015326e6c9f0cc34554ae3249ec447112eb
Klaudia <klaudia.augustynska@gmail.com>
2017-01-25 21:05:01 +0100

    otwieranie pliku

--- ludzik.hs
+++ ludzik.hs
@@ -3,12 +3,12 @@ import Data.Maybe (fromMaybe)
 import Text.ParserCombinators.Parsec
 
 data Argumenty = Argumenty
-  { nazwaPliku :: String
+  { plik :: String
   , x :: Maybe Int
   , y :: Maybe Int }
 
 data Komenda = Kolko Float Float Float
-             | Kreska Float Float Float Float
+             | Kreska Float Float Float Float deriving Show
 
 argumenty :: Options.Applicative.Parser Argumenty
 argumenty = Argumenty
@@ -25,16 +25,39 @@ argumenty = Argumenty
       <> help "Ilość pikseli od góry w pliku wynikowym" ))
 
 przechwycArgumenty :: Argumenty -> IO ()
-przechwycArgumenty (Argumenty nazwaPliku maybeX maybeY) =
+przechwycArgumenty Argumenty {plik = nazwaPliku, x = maybeX, y = maybeY} =
   let 
     x = fromMaybe 0 maybeX
     y = fromMaybe 0 maybeY
-    komendy = parsujPlik nazwaPliku
+    -- komendy = parsujPlik nazwaPliku
   in
-    print (x*y)
+    do
+      dupa <- readFile nazwaPliku
+      -- writeFile "wynik.txt" (dupa ++ "\n")
+      -- appendFile "asdf" dupa
+      print dupa
 
-parsujPlik :: String -> [Komenda]
-parsujPlik _ = [Kolko 50 100 30]
+--parsujPlik :: String -> [Komenda]
+-- parsujPlik _ = [Kolko 50 100 30]
+-- parsujPlik nazwaPliku = do
+--   dupa <- readFile "test.txt"
+--   return dupa
+  -- inh <- openFile "test.txt" ReadMode
+  -- outh <- openFile "output.txt" WriteMode
+  -- mainloop inh outh
+  -- hClose inh
+  -- hClose outh
+  -- return [Kolko 50 100 30]
+
+file = endBy line eol
+line = sepBy cell (char ' ')
+cell = Text.ParserCombinators.Parsec.many (noneOf ",\n\r")
+eol =   try (string "\n\r")
+    Text.ParserCombinators.Parsec.<|> try (string "\r\n")
+    Text.ParserCombinators.Parsec.<|> string "\n"
+    Text.ParserCombinators.Parsec.<|> string "\r"
+uzyjParseca :: String -> String -> Either Text.ParserCombinators.Parsec.ParseError [[String]]
+uzyjParseca = parse file
 
 main :: IO ()
 main = execParser opts >>= przechwycArgumenty
